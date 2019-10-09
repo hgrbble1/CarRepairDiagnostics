b75fd170f17f8b21b8295f6c672851c89f573064
Hudson <hgrbble1@memphis.edu>
2019-10-08 19:52:31 -0500

    Commiting my finished version of the code

--- src/main/java/com/ubiquisoft/evaluation/CarDiagnosticEngine.java
+++ src/main/java/com/ubiquisoft/evaluation/CarDiagnosticEngine.java
@@ -9,10 +9,33 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CarDiagnosticEngine {
 
 	public void executeDiagnostics(Car car) {
+		if (car == null) throw new IllegalArgumentException("Car must not be null");
+
+		/**Step 1
+		 * Checking For Missing Data Fields
+		 */
+		checkForMissingFields(car);
+		/**Step 2
+		 * Checking For Missing Parts
+		 */
+		checkForMissingParts(car);
+		/**Step 3
+		 * Checking For Damaged Parts
+		 */
+		checkForDamagedParts(car);
+		/**Step 4
+		 * Validation Succeeded Tell the User the Car is ready To go
+		 */
+		System.out.println("Thank you for using UbiquiSoft Car Diagnostic. \nYour car has successfully went through all of our steps and is cleared for Travel");
+
+
 		/*
 		 * Implement basic diagnostics and print results to console.
 		 *
@@ -42,23 +65,69 @@ public class CarDiagnosticEngine {
 
 	}
 
-	private void printMissingPart(PartType partType, Integer count) {
+	/**
+	 * Checking For missing data fields
+	 * If there are missing data fields then we Print out the missing data fields and exit the program.
+	 * @param car
+	 */
+	private void checkForMissingFields(Car car) {
+		List<String> missingFields = car.getMissingDataFields();
+		if(missingFields.size() > 0) {
+			System.out.println("Your car has failed our Diagnostic Test, because your car is missing field(s). The missing Field(s) are listed below:");
+			missingFields.forEach((str) -> printMissingField(str));
+			System.exit(0);
+		}
+	}
+	/**
+	 * Checking For missing Parts
+	 * If there are missing parts then we Print out the missing parts and exit the program.
+	 * @param car
+	 */
+	private void checkForMissingParts(Car car){
+		Map<PartType, Integer> missingParts = car.getMissingPartsMap();
+		if (missingParts.size() > 0) {
+			System.out.println("Your car has failed our Diagnostic Test, because your car is missing part(s). The missing Part(s) are listed below:");
+			missingParts.forEach((PartType, Integer) -> printMissingPart(PartType, Integer));
+			System.exit(0);
+		}
+	}
+	/**
+	 * Checking For damaged Parts
+	 * If there are missing parts then we Print out the damaged parts and exit the program.
+	 * @param car
+	 */
+	private void checkForDamagedParts(Car car) {
+		Map<PartType, ConditionType> damagedParts = car.getDamagedPartsMap();
+		if (damagedParts.size() > 0) {
+			System.out.println("Your car has failed our Diagnostic Test, because your car has damaged part(s). The Damaged Part(s) are listed below:");
+			damagedParts.forEach((PartType, ConditionType)-> printDamagedPart(PartType, ConditionType));
+			System.exit(0);
+		}
+	}
+
+
+	private void printMissingField (String field){
+		if (field == null) throw new IllegalArgumentException("Field must not be null");
+		System.out.println(String.format("Missing field Detected: The Car's - %s - must be filled in order to complete diagnostic check", field));
+	}
+
+	private void printMissingPart (PartType partType, Integer count){
 		if (partType == null) throw new IllegalArgumentException("PartType must not be null");
 		if (count == null || count <= 0) throw new IllegalArgumentException("Count must be greater than 0");
 
 		System.out.println(String.format("Missing Part(s) Detected: %s - Count: %s", partType, count));
 	}
 
-	private void printDamagedPart(PartType partType, ConditionType condition) {
+	private void printDamagedPart (PartType partType, ConditionType condition){
 		if (partType == null) throw new IllegalArgumentException("PartType must not be null");
 		if (condition == null) throw new IllegalArgumentException("ConditionType must not be null");
 
 		System.out.println(String.format("Damaged Part Detected: %s - Condition: %s", partType, condition));
 	}
 
-	public static void main(String[] args) throws JAXBException {
+	public static void main (String[]args) throws JAXBException {
 		// Load classpath resource
-		InputStream xml = ClassLoader.getSystemResourceAsStream("SampleCar.xml");
+		InputStream xml = ClassLoader.getSystemResourceAsStream("SampleCar3.xml");
 
 		// Verify resource was loaded properly
 		if (xml == null) {
@@ -80,5 +149,4 @@ public class CarDiagnosticEngine {
 		diagnosticEngine.executeDiagnostics(car);
 
 	}
-
 }
--- src/main/java/com/ubiquisoft/evaluation/domain/Car.java
+++ src/main/java/com/ubiquisoft/evaluation/domain/Car.java
@@ -3,6 +3,8 @@ package com.ubiquisoft.evaluation.domain;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +18,38 @@ public class Car {
 
 	private List<Part> parts;
 
+	/**
+	 * Returns a list of Missing PartType(s) and quantity of missing part.
+	 * @return
+	 */
 	public Map<PartType, Integer> getMissingPartsMap() {
+
+		Map<PartType, Integer> missingParts = createMissingPartsMap();
+
+		for (Part part : parts) {
+			switch (part.getType()) {
+				case ENGINE:
+					missingParts.remove(PartType.ENGINE);
+					break;
+				case ELECTRICAL:
+					missingParts.remove(PartType.ELECTRICAL);
+					break;
+				case TIRE:
+					if (missingParts.get(PartType.TIRE) == 1) missingParts.remove(PartType.TIRE);
+					else missingParts.replace(PartType.TIRE, missingParts.get(PartType.TIRE) - 1);
+					break;
+				case OIL_FILTER:
+					missingParts.remove(PartType.OIL_FILTER);
+					break;
+				case FUEL_FILTER:
+					missingParts.remove(PartType.FUEL_FILTER);
+					break;
+				default:
+					/**Throw an error if type is not valid**/
+					throw new IllegalArgumentException("The Enum Type was Invalid. Can only be\nENGINE,ELECTRICAL,TIRE,OIL_FILTER,FUEL_FILTER");
+			}
+		}
+		return missingParts;
 		/*
 		 * Return map of the part types missing.
 		 *
@@ -30,8 +63,92 @@ public class Car {
 		 *          "TIRE": 3
 		 *      }
 		 */
+	}
 
-		return null;
+	/**
+	 * Returns a Map of Damaged PartType(s) and the condition of the part(s)
+	 * @return
+	 */
+
+	public Map<PartType, ConditionType> getDamagedPartsMap() {
+		Map<PartType, ConditionType> damagedParts = new HashMap<PartType, ConditionType>();
+		for (Part part : parts) {
+			switch(part.getCondition()) {
+				case NEW:
+				case GOOD:
+				case WORN:
+					/** The Part is good Nothing to do Here**/
+					break;
+				case NO_POWER:
+				case USED:
+				case FLAT:
+				case SIEZED:
+				case CLOGGED:
+				case DAMAGED:
+					/** Part is damaged must be replaced**/
+					damagedParts.put(part.getType(), part.getCondition());
+					break;
+				default:
+					/** Throw an error if type is invalid.**/
+					throw new IllegalArgumentException("The Enum Type was Invalid. Can only be:\nNEW,GOOD,WORN,NO_POWER,USED,FLAT,SIEZED,CLOGGED,DAMAGED");
+			}
+		}
+		return damagedParts;
+
+		/*
+		 * Return map of the part types damaged.
+		 *
+		 * Car's damaged types are
+		 *      NO_POWER, USED, FLAT, SEIZED, CLOGGED, DAMAGED
+		 * Example: a damaged Part would be
+		 *
+		 *      {
+		 *          "TIRE": "FLAT",
+		 * 			"ELECTRICAL: "NO_POWER"
+		 *      }
+		 */
+	}
+
+	/**
+	 * Returns a list of missingDataFields if the Data Fields equal null or empty String.
+	 * @return
+	 */
+	public List<String> getMissingDataFields() {
+		List<String> missingDataFields = new ArrayList<String>();
+		if (getYear().equals(null) || getYear().equals("")) missingDataFields.add("Year");
+		if (getMake().equals(null) || getMake().equals("")) missingDataFields.add("Make");
+		if (getModel().equals(null) || getModel().equals("")) missingDataFields.add("Model");
+		return missingDataFields;
+	}
+
+	/**
+	 * For future releases I was thinking this would be great for a Schema per each type of vehicle. Such as
+	 * {
+	 *     Type: Semi-Truck (MiniVan, HatchBack, Sedan....)
+	 *     Wheels: 18,
+	 *     ENGINE: 1,
+	 *     OIL_FILTER: 1,
+	 *     FUEL_FILTER: 1,
+	 * }
+	 * {
+	 *     Type: Sedan (MiniVan, HatchBack, Sedan....)
+	 *     Wheels: 4,
+	 * 	   ENGINE: 1,
+	 * 	   OIL_FILTER: 1,
+	 * 	   FUEL_FILTER: 1,
+	 * }
+	 *
+	 * I also just thought of what if you get the type of car, year.... and then use that information to ping a Car Dealers API to figure out all of the parts the car has.
+	 * @return
+	 */
+	public Map<PartType, Integer> createMissingPartsMap() {
+		Map<PartType, Integer> missingParts = new HashMap<PartType, Integer>();
+		missingParts.put(PartType.ENGINE, 1);
+		missingParts.put(PartType.ELECTRICAL, 1);
+		missingParts.put(PartType.FUEL_FILTER, 1);
+		missingParts.put(PartType.OIL_FILTER, 1);
+		missingParts.put(PartType.TIRE, 4);
+		return missingParts;
 	}
 
 	@Override
